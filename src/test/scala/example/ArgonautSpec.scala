168c87032a0b9b5b4bc01f7cbc1315f10c652a7a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-07 19:07:02 +1100

    Wrote tests to understand Argonaut JSON library

---
+++ src/test/scala/example/ArgonautSpec.scala
@@ -0,0 +1,60 @@
+package example
+
+import argonaut._, Argonaut._
+import org.scalatest.{FlatSpec, Matchers}
+
+class ArgonautSpec extends FlatSpec with Matchers {
+
+  case class TestObject1(key: String)
+
+  implicit def TestObjectDecodeJson: DecodeJson[TestObject1] = jdecode1L(TestObject1.apply)("key")
+
+  def decodeObject1(inputJson: String) = inputJson.decodeOption[TestObject1]
+
+  "Argonaut" should "parse JSON object with single key" in {
+    val input = """{"key":"value"}"""
+    decodeObject1(input) match {
+      case Some(TestObject1(key)) => key shouldEqual "value"
+    }
+  }
+
+  "Argonaut" should "ignores the key the result object does not mention" in {
+    val input = """{"key":"value","unknownKey":"unknownValue"}"""
+    decodeObject1(input) match {
+      case Some(TestObject1(key)) => key shouldEqual "value"
+    }
+  }
+
+
+
+  case class TestObject2(key1: String, key2: Int)
+
+  implicit def TestObject2DecodeJson: DecodeJson[TestObject2] = jdecode2L(TestObject2.apply)("key1", "key2")
+
+  def decodeObject2(inputJson: String) = inputJson.decodeOption[TestObject2]
+
+  "Argonaut" should "parse JSON object with multiple keys" in {
+    val input = """{"key1":"value1","key2":2}"""
+    decodeObject2(input) match {
+      case Some(TestObject2(_, key2)) => key2 shouldEqual 2
+    }
+  }
+
+
+
+  def decodeObjectList1(inputJson: String) = inputJson.decodeOption[List[TestObject1]]
+
+  "Argonaut" should "automatically parse an array of the same object" in {
+    val input = """[{"key":"value"}]"""
+    decodeObjectList1(input) match {
+      case Some(List(TestObject1(key))) => key shouldEqual "value"
+    }
+  }
+
+
+//  // https://stackoverflow.com/questions/40751321/argonaut-decoding-a-polymorphic-array/40753319#40753319
+//  "Argonaut" should "parse an array who contains different type of elements" in {
+//    val input = """[1, "A"]"""
+//  }
+
+}
