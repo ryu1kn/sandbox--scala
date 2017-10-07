e3344e99c80fd2835cdb237e54cb80e87068442c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-07 21:22:17 +1100

    Test for reading a file

---
+++ src/test/scala/example/SourceSpec.scala
@@ -0,0 +1,18 @@
+package example
+
+import org.scalatest._
+
+class SourceSpec extends FlatSpec with Matchers {
+
+  "Source.fromFile" should "read a file" in {
+    val path = "src/test/resources/file-reader--1.txt"
+    readFile(path) shouldEqual "Hello\nWorld"
+  }
+
+  def readFile(path: String): String = {
+    val source = io.Source.fromFile(path)
+    try source.getLines.mkString("\n")
+    finally source.close
+  }
+
+}
